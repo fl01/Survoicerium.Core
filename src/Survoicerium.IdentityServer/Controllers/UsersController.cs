644e0ec2ae219ed7a605fdfbee2bac70ab883a4a
Artem Vasylenko <artemjjke@gmail.com>
2017-10-23 01:22:03 +0200

    Added identityserver

---
+++ src/Survoicerium.IdentityServer/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Survoicerium.IdentityServer.Controllers
+{
+    public class ValuesController : Controller
+    {
+        [HttpGet("api/me")]
+        public IEnumerable<string> GetMe()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
