a9efa23dded46d892bbe0fedae26caefef2fa5f4
Artem Vasylenko <artemjjke@gmail.com>
2017-10-22 21:03:21 +0200

    cleanup

---
+++ src/Survoicerium.Frontend/Pages/DiscordOAuth.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Survoicerium.Frontend.Pages
+{
+    public class DiscordOAuthModel : PageModel
+    {
+        public string Token { get; set; }
+
+        public void OnGet([FromQuery]string token)
+        {
+            Token = token;
+        }
+    }
+}
