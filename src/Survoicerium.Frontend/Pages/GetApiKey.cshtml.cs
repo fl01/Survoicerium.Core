a39ea8c11565a72da8d407aae53abecf68d3f39e
Artem Vasylenko <artemjjke@gmail.com>
2017-10-23 02:40:32 +0200

    added dummy page - GetApiKey

---
+++ src/Survoicerium.Frontend/Pages/GetApiKey.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Survoicerium.Frontend.Pages
+{
+    public class GetApiKey : PageModel
+    {
+        public async void OnGet([FromQuery]string hardwareId)
+        {
+            var r = Redirect("http://google.com");
+            await r.ExecuteResultAsync(PageContext);
+        }
+    }
+}
