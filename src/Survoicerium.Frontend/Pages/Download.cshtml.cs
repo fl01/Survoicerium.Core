31dbf6186353c3bd3d54d13fd6c55d053d8e5cd0
Artem Vasylenko <artemjjke@gmail.com>
2017-12-09 00:39:15 +0200

    redisign layout, faq page

---
+++ src/Survoicerium.Frontend/Pages/Download.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Survoicerium.Frontend.Pages
+{
+    public class DownloadModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ src/Survoicerium.Frontend/Pages/Info.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Survoicerium.Frontend.Pages
+{
+    public class InfoModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
